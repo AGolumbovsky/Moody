fce527054cecda4ac38d63a917bf1632f53a57c4
AGolumbovsky <agolumbovsky@gmail.com>
2015-06-19 22:13:02 -0600

    some endpoints

--- server.js
+++ server.js
@@ -15,7 +15,20 @@ var port = 8888;
 mongoose.connect('mongodb://localhost/moody');
 var db = mongoose.connection;
 
+
+//endpoints
+app.post('moods', function(req, res) {
+	var mood = new Mood(req.body);
+	mood.save(function(err, moo) {
+		if (err) {
+			console.log("something's wrong, you should be sad");
+		}
+		res.json(moo);
+	})
+})
+
+
 //listen on port
 app.listen(port, function() {
 	console.log('being all moody on port', port);
-})
+});

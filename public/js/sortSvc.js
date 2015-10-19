0c3902ac55541b707a6d49d8bb39d32e1586ad47
AGolumbovsky <agolumbovsky@gmail.com>
2015-10-18 20:31:46 -0700

    added pokerFace as background

--- public/js/sortSvc.js
+++ public/js/sortSvc.js
@@ -1,8 +1,18 @@
 var app = angular.service('moody');
 
-app.service('sortSvc', function() {
+app.service('sortSvc', function(mainSvc) {
 
-this.byWeek = function(rawData) {
+// what do you want it to do, man?
+this.byWeekday = function(rawData) {
+
+	var displayWeekday = '';
+
+
+	// find the best way to display the days returned from server
+	// ***********!!!!!!!!!!!!!!!!!!!
+	if (rawData.weekday === 0) displayWeekday = "Sunday";
+	else if (rawData.weekday === 1) displayWeekday = "Monday";
+	// and so on...
 
 }
 

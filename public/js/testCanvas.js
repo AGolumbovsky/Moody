dcec3b9f7a0ce2a344ec36de917ad4798038f840
AGolumbovsky <agolumbovsky@gmail.com>
2015-10-26 00:55:30 -0700

    i can smile now...manually. Yay!!!

--- public/js/testCanvas.js
+++ public/js/testCanvas.js
@@ -3,11 +3,21 @@ function draw() {
 		var canvas = document.getElementById('drawGrin');
 		var ctx = canvas.getContext('2d');
 
-			ctx.fillStyle = "rgb(200,0,0)";
-			ctx.fillRect (10, 10, 55, 50);
+		// provide for changing the curve parameters dynamically
+		//var cornerStartX = 25;
+		//var cornerStartY = 75;
+		//var cornerEndX = 300 - cornerStartX; //canvas width default = 300
+		//var cornerEndY = cornerStartY; // maybe redundant
 
-			ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-			ctx.fillRect (30, 30, 55, 50);
+		// make this respond to average moods 0 -- 1 to 0 -- 150 pts
+		var ctrlPtY = 100; // default for neutral
+
+		//draw the grin
+		ctx.lineWidth = 8;
+		ctx.beginPath();
+		ctx.moveTo(25, 75);
+		ctx.quadraticCurveTo(150, ctrlPtY, 275, 75);
+		ctx.stroke();
 
 	};	
 
